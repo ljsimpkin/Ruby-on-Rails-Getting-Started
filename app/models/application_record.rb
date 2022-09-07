4f385f4af32aa784f75e00f3fa119a2a3326659e
Liam Simpkin <lsimpkin@comprehensivecare.co.nz>
2022-09-08 11:16:11 +1200

    add comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
+
+class Article < ApplicationRecord
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
