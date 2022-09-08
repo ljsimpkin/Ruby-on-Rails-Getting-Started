c868ebf1e36d11f960d1b2b41cc939fa6691a55f
Liam Simpkin <lsimpkin@comprehensivecare.co.nz>
2022-09-08 13:05:06 +1200

    add status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,23 @@
+# class Article < ApplicationRecord
+#   has_many :comments
+
+#   validates :title, presence: true
+#   validates :body, presence: true, length: { minimum: 10 }
+
+#   VALID_STATUSES = ['public', 'private', 'archived']
+
+#   validates :status, inclusion: { in: VALID_STATUSES }
+
+#   def archived?
+#     status == 'archived'
+#   end
+# end
+
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
-  end
-  
+  include Visible
+
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
